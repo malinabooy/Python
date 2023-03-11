ac3184abe72a023485cb1d001ab5e4e84c696b9a
malinabooy <1anejlbcuh4uk1@gmail.com>
2023-03-11 16:10:52 +0300

    First

---
+++ Seminars/Seminar1/Sem1.py
@@ -0,0 +1,12 @@
+a = int(input("число А "))
+
+b = int(input("число Б "))
+
+if a > b:
+    print(f"наибольшее число a = {a}")
+
+elif a == b:
+    print(f"Они равны")
+
+else:
+    print(f"наибольшее число b = {b}")
